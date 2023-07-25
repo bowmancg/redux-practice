58e82cb8e14383bca95681d1499cace4114acd7a
Chandler Bowman <doctorgrant99@gmail.com>
2023-07-25 15:20:43 -0600

    adding useSlice

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,39 +1,9 @@
-import redux, { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+import counterSlice from './counter-slice'
+import authSlice from './auth-slice'
 
-const initialState = { counter: 0, showCounter: true }
-
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
-    }
-
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
-
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-
-    if (action.type === 'toggle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-
-    return state
-}
-
-const store = createStore(counterReducer)
+const store = configureStore({
+    reducer: { counter: counterSlice, auth: authSlice },
+})
 
 export default store
